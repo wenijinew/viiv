6eb8360989b960ff9d32eda78885a14e0f78a565
Bruce Wen <wenijinew@gmail.com>
2023-12-11 13:49:20 +0100

    Tune Colors, Re-generate Blue Theme

--- viiv.ps1
+++ viiv.ps1
@@ -14,7 +14,7 @@ $GRADATIONAL_TOTAL = 60
 $DARK_COLOR_GRADATIONAL_TOTAL = 60 
 $GENERAL_MAX_COLOR = 100 
 $DARK_MAX_COLOR = 20
-$DARK_BASE_COLORS = "['#0c0c0c']"
+$DARK_BASE_COLORS = "['#00001b']"
 
 # Generate palette colors
 Function GeneratePaletteColors { 
