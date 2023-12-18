5472a214f18521c1391ca3f90e740521c1a0199e
Bruce Wen <wenijinew@gmail.com>
2023-12-18 06:21:29 +0100

    Divide dark colors and light colors in all base colors. Use 1-7 as light colors, and 8-11 as dark colors which are mainly used for background

--- viiv.ps1
+++ viiv.ps1
@@ -9,18 +9,19 @@ $DYNAMIC_THEME_NAME = "dynamic"
 $THEME_FILE_EXTENSION = "-color-theme.json"
 $DYNAMIC_PALETTE_FILENAME = "dynamic-palette.txt"
 $TEMPLATE_THEME_FILENAME = "viiv-color-theme.template.json"
-$BASE_COLOR_TOTAL = 10
+$BASE_COLOR_TOTAL = 7
 $GRADATIONAL_TOTAL = 60 
 $DARK_COLOR_GRADATIONAL_TOTAL = 60 
 $GENERAL_MIN_COLOR = 20 
 $GENERAL_MAX_COLOR = 120 
 $DARK_MIN_COLOR = 0
 $DARK_MAX_COLOR = 10
-$DARK_BASE_COLORS = "['#0d0b12']"
+$DARK_COLORS_TOTAL = 4
+$DARK_BASE_COLORS = "[]"
 
 # Generate palette colors
 Function GeneratePaletteColors { 
-	$palette = python -c "from peelee.peelee import Palette; palette = Palette($BASE_COLOR_TOTAL, $GRADATIONAL_TOTAL, $DARK_COLOR_GRADATIONAL_TOTAL, $GENERAL_MIN_COLOR, $GENERAL_MAX_COLOR, $DARK_MIN_COLOR, $DARK_MAX_COLOR, $DARK_BASE_COLORS).generate_palette(); print(palette)"
+	$palette = python -c "from peelee.peelee import Palette; palette = Palette($BASE_COLOR_TOTAL, $GRADATIONAL_TOTAL, $DARK_COLOR_GRADATIONAL_TOTAL, $GENERAL_MIN_COLOR, $GENERAL_MAX_COLOR, $DARK_MIN_COLOR, $DARK_MAX_COLOR, $DARK_COLORS_TOTAL, $DARK_BASE_COLORS).generate_palette(); print(palette)"
 	Select-String -Pattern 'C_\d{2}_\d{2}:#[A-Fa-f0-9]{6}' -InputObject $palette -AllMatches | ForEach-Object {$_.Matches} | ForEach-Object {$_.Value} | Set-Content "$DIR\themes\$DYNAMIC_PALETTE_FILENAME"
 }
 
