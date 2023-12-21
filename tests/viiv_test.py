eaa65abb40d139b925cd08915d3ca80785151161
Bruce Wen <wenijinew@gmail.com>
2023-12-21 20:56:42 +0100

    Tuning diffEditor

--- tests/viiv_test.py
+++ tests/viiv_test.py
@@ -1,28 +1,26 @@
-#!/usr/bin/env python3
-
 import unittest
-from my_module import ColorComponent
 
-class TestColorMethods(unittest.TestCase):
+from viiv.viiv import _random_range
 
-    def test_append_or_replace_alpha(self):
-        old_color = "#FFB6C1"
-        new_color = "#00FF7F"
-        
-        # Test appending alpha component
-        alpha_color = ColorComponent.ALPHA
-        expected_alpha_result = "#00FF7FFF"
-        self.assertEqual(_append_or_replace_alpha(old_color, new_color, alpha_color), expected_alpha_result)
 
-        # Test replacing light component
-        light_color = ColorComponent.LIGHT
-        expected_light_result = "#00FF7F01"
-        self.assertEqual(_append_or_replace_alpha(old_color, new_color, light_color), expected_light_result)
+class TestRandomRange(unittest.TestCase):
+    def test_random_range_start_end_less_than_10(self):
+        result = _random_range(["1", "12"])
+        self.assertEqual(
+            result,
+            ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"],
+        )
 
-        # Test replacing basic component
-        basic_color = ColorComponent.BASIC
-        expected_basic_result = "#00B6C1"
-        self.assertEqual(_append_or_replace_alpha(old_color, new_color, basic_color), expected_basic_result)
+    def test_random_range_start_end_greater_than_10(self):
+        result = _random_range(["11", "20"])
+        self.assertEqual(
+            result, ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+        )
 
-if __name__ == '__main__':
+    def test_random_range_start_end_equal(self):
+        result = _random_range(["10", "10"])
+        self.assertEqual(result, ["10"])
+
+
+if __name__ == "__main__":
     unittest.main()
