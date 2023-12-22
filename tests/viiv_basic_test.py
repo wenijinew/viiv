021d28b68c85415611924c92e2caad5a13224046
Bruce Wen <wenijinew@gmail.com>
2023-12-22 13:48:33 +0100

    Set decoration group and use stable basic range to have the same color for decoration groups

--- tests/viiv_basic_test.py
+++ tests/viiv_basic_test.py
@@ -1,24 +1,24 @@
 import unittest
 
-from viiv.viiv import _random_range
+from viiv.viiv import normalize_range
 
 
 class TestRandomRange(unittest.TestCase):
     def test_random_range_start_end_less_than_10(self):
-        result = _random_range(["1", "12"])
+        result = normalize_range(["1", "12"])
         self.assertEqual(
             result,
             ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"],
         )
 
     def test_random_range_start_end_greater_than_10(self):
-        result = _random_range(["11", "20"])
+        result = normalize_range(["11", "20"])
         self.assertEqual(
             result, ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
         )
 
     def test_random_range_start_end_equal(self):
-        result = _random_range(["10", "10"])
+        result = normalize_range(["10", "10"])
         self.assertEqual(result, ["10"])
 
 
