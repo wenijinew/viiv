62fe2daabbb100d28efcc464313fa2fdcc4fc55e
Bruce Wen <wenijinew@gmail.com>
2024-01-08 11:06:11 +0100

    Add new DARK themes to package.json.

--- tests/viiv_package_test.py
+++ tests/viiv_package_test.py
@@ -14,8 +14,25 @@ class TestPackage(unittest.TestCase):
         package_file_path = root_path + "package.json"
         package_data = load_json_file(package_file_path)
         themes = package_data["contributes"]["themes"]
+        theme_filenames_in_package = []
         for theme in themes:
             theme_path = theme["path"]
-            assert os.path.exists(
-                f"{root_path}{theme_path}"
-            ), f"{theme_path} doesn't exist."
+            theme_filename = theme_path.split("/")[-1]
+            theme_full_path = root_path + theme_path
+            theme_filenames_in_package.append(theme_filename)
+            assert os.path.exists(theme_full_path), f"{theme_path} doesn't exist."
+        themes_path_root = root_path + "themes"
+        theme_filenames_in_root_path = list(
+            filter(
+                lambda x: x.endswith("color-theme.json"),
+                os.listdir(themes_path_root),
+            )
+        )
+        themes_not_in_package = [
+            f"{themes_path_root}{os.sep}{x}"
+            for x in theme_filenames_in_root_path
+            if x not in theme_filenames_in_package
+        ]
+        assert (
+            len(themes_not_in_package) == 0
+        ), f"Themes not in package: {themes_not_in_package}"
